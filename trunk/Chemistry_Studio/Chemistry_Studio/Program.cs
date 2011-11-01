88d2f99a22fcec6120c6977bd908c8cc133d8bb6
abhi.guddu <abhi.guddu@436548fe-53d2-545b-0b9e-c8df33a7e872>
2011-11-01 06:13:57 +0000

    XML parsing of questions and options done - Abhishek

--- trunk/Chemistry_Studio/Chemistry_Studio/Program.cs
+++ trunk/Chemistry_Studio/Chemistry_Studio/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Xml;
 namespace Chemistry_Studio
 {
     //Structure to contain positions and confidences of tokens obtained during parse
@@ -25,6 +25,54 @@ namespace Chemistry_Studio
             confidences.Add(confidence);
         }
     }
+
+    class Question_Struct
+    {
+        string id;
+        string question;
+        List<string> options;
+        
+        public Question_Struct()
+        {
+            options=new List<string>();
+        }
+        public Question_Struct(string fileName)
+        {
+            options=new List<string>();
+            XmlDocument xmlDoc = null;
+            // Setting the XmlReaderSettings so as to ignore Comments from the Input XML file.
+            XmlReaderSettings readerSettings = new XmlReaderSettings();
+            readerSettings.IgnoreWhitespace = true;
+            readerSettings.IgnoreComments = true;
+
+            using (XmlReader reader = XmlReader.Create(fileName, readerSettings))
+            {
+                xmlDoc = new XmlDocument();
+                xmlDoc.Load(reader);
+            }
+            XmlNode questionTag = xmlDoc.FirstChild; // Skipping the root node.
+            this.id = questionTag.ChildNodes[0].InnerText;
+            this.question = questionTag.ChildNodes[1].InnerText;
+            
+            for(int i=2;i<questionTag.ChildNodes.Count;i++)
+            {
+                options.Add(questionTag.ChildNodes[i].InnerText);
+            }
+        }
+
+        public override string ToString()
+        {
+            string representation = this.id+") ";
+            representation += this.question + "\n\n";
+            for (int i = 0; i < options.Count; i++)
+            {
+                representation += (i+1).ToString() + ". " + options[i] + "\n";
+            }
+            return representation;
+        }
+
+        }
+
     
     class Program
     {
@@ -545,8 +593,13 @@ namespace Chemistry_Studio
         {
             Tokens.initialize();
             Tokens.initializePredSpec();
-            
-            
+            string question_path = "C:\\Users\\Abhishek\\Documents\\Visual Studio 2010\\Projects\\Chemistry_Studio\\Chemistry_Studio\\Chemistry_Studio\\Questions\\";
+            Question_Struct q1=new Question_Struct(question_path+"Q1.txt");
+            Question_Struct q2 = new Question_Struct(question_path + "Q2.txt");
+            Question_Struct q3 = new Question_Struct(question_path + "Q3.txt");
+            Console.WriteLine(q1);
+            Console.WriteLine(q2);
+            Console.WriteLine(q3);
             string sentence = "";
             foreach (string str in args)
                 sentence += " " + str;
@@ -647,7 +700,7 @@ namespace Chemistry_Studio
                 Console.WriteLine("Program Crashed! with message : " + e.ToString());
             }
 
-            Console.WriteLine(completeTrees[0].XMLForm());
+            //Console.WriteLine(completeTrees[0].XMLForm());
             Console.ReadLine();
             
         }
