f7eaeea636ec663afdf5856f157266043aa5e598
abhi.guddu <abhi.guddu@436548fe-53d2-545b-0b9e-c8df33a7e872>
2011-11-01 10:56:58 +0000

    Same(NumPred, NumPred) working - AK

---
+++ Chemistry_Studio/Chemistry_Studio/Position_Confidence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chemistry_Studio
+{
+    class Position_Confidence
+    {
+        public List<double> confidences;
+        public List<int> positions;
+
+        public Position_Confidence(double confidence, int position)
+        {
+            confidences = new List<double>();
+            positions = new List<int>();
+            confidences.Add(confidence);
+            positions.Add(position);
+        }
+
+        public void add(double confidence, int position)
+        {
+            positions.Add(position);
+            confidences.Add(confidence);
+        }
+
+        public void remove(int position)
+        {
+            for (int i = 0; i < this.positions.Count; i++)
+            {
+                if (positions[i] == position)
+                {
+                    positions.RemoveAt(i);
+                    confidences.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+
+
+    }
+}
